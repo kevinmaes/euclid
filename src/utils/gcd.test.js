0dd9ce022117b7f6bd235f1228a70ec96303a20c
Kevin Maes <kevin@kevinmaes.com>
2018-06-03 20:40:10 -0400

    add tests for calcGCDSquares

--- src/utils/gcd.test.js
+++ src/utils/gcd.test.js
@@ -1,4 +1,4 @@
-import { gcd, gcdSteps } from './gcd';
+import { gcd, gcdSteps, calcGCDSquares } from './gcd';
 
 describe('gcd()', () => {
   it('should return the greatest common divisor when a % b is 0', () => {
@@ -11,7 +11,6 @@ describe('gcd()', () => {
     const result = gcd(10, 4);
 
     expect(result).toBe(2);
-
   });
 
   it('should return the greatest common divisor when a and b have no gcd other than 1', () => {
@@ -62,5 +61,64 @@ describe('gcdSteps()', () => {
 
     expect(result.length).toBe(8);
     expect(result[result.length - 1].gcd).toBe(1);
+  });
+});
+
+describe('calcGCDSquares()', () => {
+  it('should return a default object if there are no steps passed in', () => {
+    const steps = [];
+    const inputs = [];
+
+    const result = calcGCDSquares(steps, inputs);
+
+    expect(result).toEqual({ gcd: 0, totalSquares: 0 });
+  });
+
+  it('should return an object with gcd and totalSquares if there is 1 step', () => {
+    const steps = [
+      {
+        gcd: 1,
+      },
+    ];
+    const inputs = [1, 5];
+
+    const result = calcGCDSquares(steps, inputs);
+
+    expect(result).toEqual({ gcd: 1, totalSquares: 5 });
+  });
+
+  it('should return an object with gcd and totalSquares if there are 2 steps', () => {
+    const steps = [
+      {
+        gcd: null,
+      },
+      {
+        gcd: 1,
+      },
+    ];
+    const inputs = [5, 2];
+
+    const result = calcGCDSquares(steps, inputs);
+
+    expect(result).toEqual({ gcd: 1, totalSquares: 10 });
+  });
+
+  it('should return an object with gcd and totalSquares if there are 3 or more steps', () => {
+    const steps = [
+      {
+        gcd: null,
+      },
+      {
+        gcd: null,
+      },
+      {
+        gcd: 15,
+      },
+    ];
+    const inputs = [345, 150];
+
+    const result = calcGCDSquares(steps, inputs);
+
+    expect(result).toEqual({ gcd: 15, totalSquares: 230 });
   });
 });
